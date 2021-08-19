1fbeba559f6ab3a9c623abca544dacc65a14c7e4
Justin <contact@tsujin.com>
2021-08-19 15:36:13 -0400

    fixed typo in pickel model name

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict_csv():
         f = request.files['file']
         csv_data = np.genfromtxt(f, delimiter=',')
 
-        if not path.exists("./models/rfclassifer.joblib"):
+        if not path.exists("./models/rfclassifier.joblib"):
             print("Model not found, creating new one")
             model_predictor = rfclassifier.PredictorModel()
             model_predictor.save_model()
@@ -40,7 +40,7 @@ def predict_csv():
 
         else:
             print("Loading saved model")
-            model_predictor = rfclassifier.PredictorModel(model='./models/rfclassifer.joblib')
+            model_predictor = rfclassifier.PredictorModel(model='./models/rfclassifier.joblib')
             prediction = model_predictor.predict(csv_data.reshape(1, -1))
 
         pred_int = int(prediction)
